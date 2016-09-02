b268505c02d408a35907916acd4e4112978f1298
jonathanchai <a>
2016-09-02 08:40:51 -0700

    Square + Cube

---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,13 @@
+
+public class Calculate {
+
+		public static double square (double x){
+			double answer = x*x;
+			return answer;
+		}
+		
+		public static double cube (double x){
+			double answer = x*x*x;
+			return answer;
+		}
+}
---
+++ Java/src/Java.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jonathan Chai
+ * Date: 08/30/2016
+ */
+public class Java {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
